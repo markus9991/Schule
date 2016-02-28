1e0c86fa2cb5fcb6a11e05142cd5c2b522ef3149
Markus <Makus.Egger@students.htlinn.ac.at>
2016-02-28 22:15:55 +0100

    Sonnenzeiten hochgeladen

---
+++ Sonnenzeiten/app/src/main/java/com/example/markus/sonnenzeiten/MainActivity.java
@@ -0,0 +1,150 @@
+package com.example.markus.sonnenzeiten;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private String url = "http://api.openweathermap.org/data/2.5/weather?q=Innsbruck&appid=44db6a862fba0b067b1930da0d769e98";
+    private TextView tv1;
+    private String wetterdaten;
+    private EditText dateauf, dateunter;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        dateauf = (EditText) findViewById(R.id.dateauf);
+        dateunter = (EditText) findViewById(R.id.dateunter);
+
+    }
+
+    // When user clicks button, calls AsyncTask.
+    // Before attempting to fetch the URL, makes sure that there is a network connection.
+    public void myClickHandler(View view) {
+        // Gets the URL from the UI's text field.
+        String stringUrl = url;
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            tv1.setText("No network connection available.");
+        }
+    }
+
+
+    // Uses AsyncTask to create a task away from the main UI thread. This task takes a
+    // URL string and uses it to create an HttpUrlConnection. Once the connection
+    // has been established, the AsyncTask downloads the contents of the webpage as
+    // an InputStream. Finally, the InputStream is converted into a string, which is
+    // displayed in the UI by the AsyncTask's onPostExecute method.
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            wetterdaten = result;
+            tv1.setText(wetterdaten);
+            dateauf.setText(timeStamptoDate(getInformation("sunrise")));
+            dateunter.setText(timeStamptoDate(getInformation("sunset")));
+        }
+    }
+
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the web page content as a InputStream, which it returns as
+    // a string.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+    private String getInformation(String info) {
+        try {
+            JSONObject jobj = new JSONObject(wetterdaten);
+            JSONObject coordObj = jobj.getJSONObject("sys");
+            return coordObj.getString(info);
+        } catch (JSONException exp) {
+            exp.printStackTrace();
+            return "Error JSON failed";
+        }
+    }
+
+    private String timeStamptoDate(String timestamp) {
+        long dv = Long.valueOf(timestamp)*1000;// its need to be in milisecond
+        Date df = new java.util.Date(dv);
+        return new SimpleDateFormat("dd.MM.yyyy kk:mm").format(df);
+
+
+
+
+    }
+}
